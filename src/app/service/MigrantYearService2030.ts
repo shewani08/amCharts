e3b4544b6cf5849ec117d37ffc1c354900cced9a
Rohit Nautiyal <rohit@baysconsulting.co.uk>
2024-01-17 11:59:22 +0000

    update sheets

--- src/app/service/MigrantYearService2030.ts
+++ src/app/service/MigrantYearService2030.ts
@@ -9,39 +9,39 @@ export class MigrantYearService {
   constructor(private http: HttpClient) { }
  
   getWaterStress(){
-    return this.http.get('assets/CURRENT_STATES/climate_scenarios/2030/Water stress index_SSP1_2030.csv', { responseType: 'text' });
+    return this.http.get('assets/CURRENT_STATES/climate_scenarios/2030/Water_stress_index_SSP1_2030.csv', { responseType: 'text' });
   }
   getWaterStressSSP2(){
-    return this.http.get('assets/CURRENT_STATES/climate_scenarios/2030/Water stress index_SSP2_2030.csv', { responseType: 'text' });
+    return this.http.get('assets/CURRENT_STATES/climate_scenarios/2030/Water_stress_index_SSP2_2030.csv', { responseType: 'text' });
   }
   getWaterStressSSP3(){
-    return this.http.get('assets/CURRENT_STATES/climate_scenarios/2030/Water stress index_SSP3_2030.csv', { responseType: 'text' });
+    return this.http.get('assets/CURRENT_STATES/climate_scenarios/2030/Water_stress_index_SSP3_2030.csv', { responseType: 'text' });
   }
   getHeatMigrantData(){
-    return this.http.get('assets/CURRENT_STATES/climate_scenarios/2030/Heat stress event exposure_SSP1_2030.csv', { responseType: 'text' });
+    return this.http.get('assets/CURRENT_STATES/climate_scenarios/2030/Heat_stress_event_exposure_SSP1_2030.csv', { responseType: 'text' });
   }
   getHeatMigrantDataSSP2(){
-    return this.http.get('assets/CURRENT_STATES/climate_scenarios/2030/Heat stress event exposure_SSP2_2030.csv', { responseType: 'text' });
+    return this.http.get('assets/CURRENT_STATES/climate_scenarios/2030/Heat_stress_event_exposure_SSP2_2030.csv', { responseType: 'text' });
   }
   getHeatMigrantDataSSP3(){
-    return this.http.get('assets/CURRENT_STATES/climate_scenarios/2030/Heat stress event exposure_SSP3_2030.csv', { responseType: 'text' });
+    return this.http.get('assets/CURRENT_STATES/climate_scenarios/2030/Heat_stress_event_exposure_SSP3_2030.csv', { responseType: 'text' });
   }
   getCropYieldMigrantData(){
-    return this.http.get('assets/CURRENT_STATES/climate_scenarios/2030/Crop yield change_SSP1_2030.csv', { responseType: 'text' });
+    return this.http.get('assets/CURRENT_STATES/climate_scenarios/2030/Crop_yield_change_SSP1_2030.csv', { responseType: 'text' });
   }
   getCropYieldMigrantDataSSP2(){
-    return this.http.get('assets/CURRENT_STATES/climate_scenarios/2030/Crop yield change_SSP2_2030.csv', { responseType: 'text' });
+    return this.http.get('assets/CURRENT_STATES/climate_scenarios/2030/Crop_yield_change_SSP2_2030.csv', { responseType: 'text' });
   }
   getCropYieldMigrantDataSSp3(){
-    return this.http.get('assets/CURRENT_STATES/climate_scenarios/2030/Crop yield change_SSP3_2030.csv', { responseType: 'text' });
+    return this.http.get('assets/CURRENT_STATES/climate_scenarios/2030/Crop_yield_change_SSP3_2030.csv', { responseType: 'text' });
   }
   getDroughtMigrantData(){
-    return this.http.get('assets/CURRENT_STATES/climate_scenarios/2030/Drought intensity change_SSP1_2030.csv', { responseType: 'text' });
+    return this.http.get('assets/CURRENT_STATES/climate_scenarios/2030/Drought_intensity_change_SSP1_2030.csv', { responseType: 'text' });
   }
   getDroughtMigrantDataSSP2(){
     return this.http.get('assets/CURRENT_STATES/climate_scenarios/2030/Drought intensity change_SSP2_2030.csv', { responseType: 'text' });
   }
   getDroughtMigrantDataSSP3(){
-    return this.http.get('assets/CURRENT_STATES/climate_scenarios/2030/Drought intensity change_SSP3_2030.csv', { responseType: 'text' });
+    return this.http.get('assets/CURRENT_STATES/climate_scenarios/2030/Drought_intensity_change_SSP3_2030.csv', { responseType: 'text' });
   }
 }

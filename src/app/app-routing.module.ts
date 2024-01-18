1d5682508dbd5750a30e6c2c84b570147ca5b538
Rohit Nautiyal <rohit@baysconsulting.co.uk>
2024-01-18 12:07:09 +0000

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { NodeLinkComponent } from './node-link/node-link.component';
+// import { NodeLinkComponent } from './node-link/node-link.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { LoginComponent } from './login/login.component';
+//import { LoginComponent } from './login/login.component';
 import { DemonavigationComponent } from './demonavigation/demonavigation.component';
 import { UserGuideComponent } from './user-guide/user-guide.component';
-import { AppComponent } from './app.component';
+//import { AppComponent } from './app.component';
 const routes: Routes = [
   //{ path: 'dashboard', component: DashboardComponent },
   { path: '', component: DemonavigationComponent }, // Redirect to home if no path specified
--- src/app/user-guide/user-guide.component.ts
+++ src/app/user-guide/user-guide.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import jsPDF from 'jspdf';
 
 @Component({
   selector: 'app-user-guide',
   templateUrl: './user-guide.component.html',
   styleUrls: ['./user-guide.component.css']
 })
-export class UserGuideComponent {
+export class UserGuideComponent implements OnInit {
 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  downloadPdf() {
+    const doc = new jsPDF();
+
+    // Optionally, you can use HTML content to generate the PDF
+    // For complex layouts, it's better to manually set the content using jsPDF's API
+    doc.text("User Guide Manual", 10, 10);
+    // Add more content here...
+
+    // Save the PDF
+    doc.save('UserGuide.pdf');
+  }
 }
